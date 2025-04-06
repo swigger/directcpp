833289b524d6aeeeaab61f8521076d08bdd08326
lixungeng <lixungeng@corp.wendao.fund>
2025-04-06 11:16:22 +0800

    update rust2h.py

--- tools/rust2h.py
+++ tools/rust2h.py
@@ -5,10 +5,12 @@
 # some format of rust structures into C++ struct while keeping bit by bit compatiable.
 # Use it at your OWN RISK and you can extend it as you need.
 # the #[repr(C)] attribute is required to make a rust struct being able to be compatiable with C struct.
+
 import re
 import sys
 import argparse
 import io
+import bisect
 
 
 class MyParser:
@@ -25,15 +27,26 @@ class MyParser:
         self.rules = []
         self.op_map[';'] = ';'
         self.tokens = []
+        self.comments = []
+        self.current_comment = []
         beg1 = 0
+        last_token = None
         while True:
             name, ss, len1 = token_func(instr)
             if name == "EOF":
                 break
-            if name in ['SPACE', "NEWLINE", "COMMENT", "COMMENT1", "COMMENT2", "COMMENT3"]:
+            if name in ['SPACE', "NEWLINE"]:
+                if name == 'NEWLINE':
+                    last_token = None  # when a new line starts, the comment should attach to the next token.
                 beg1 += len1
                 instr = instr[len1:]
                 continue
+            if name in {"COMMENT", "COMMENT1", "COMMENT2", "COMMENT3"}:
+                self.comments.append([last_token if last_token is not None else len(self.tokens), ss])
+                beg1 += len1
+                instr = instr[len1:]
+                continue
+            last_token = len(self.tokens)  # mark position of last important token
             if name == "KEYWORD":
                 self.con_str += self._char4kw(ss)
             elif name == "OPERATOR":
@@ -54,15 +67,8 @@ class MyParser:
             beg1 += len1
             instr = instr[len1:]
         # load rules.
-        rl_index = 0
-        for rl in rules:
-            if isinstance(rl, list):
-                rl_meta = rl[1:]
-                rl = rl[0]
-            else:
-                rl_meta = rl_index
+        for rl_meta, rl in rules.items():
             assert isinstance(rl, str)
-            rl_index += 1
             self._parse_rule(rl, rl_meta)
         self.all_keywords = "".join(self.kw_map.values())
         self.all_operators = "".join(self.op_map.values())
@@ -97,7 +103,7 @@ class MyParser:
                 rl = rl[m.end():]
             elif m := re.match(r"\s+", rl):
                 rl = rl[m.end():]
-            elif m := re.match(r"[(){}\[\]|+\-?*:0-9,]", rl):
+            elif m := re.match(r"[(){}\[\]|+\-?*:,.=0-9]+", rl):
                 regex += m.group()
                 rl = rl[m.end():]
             else:
@@ -147,11 +153,17 @@ class MyParser:
                 retss.append(self._back2str(precnt + m.start(i+1), m.group(i+1)))
         return retss
 
+    def _load_comments(self, start, end):
+        s1 = bisect.bisect_left(self.comments, start, key=lambda x: x[0])
+        e1 = bisect.bisect_right(self.comments, end, lo=s1, key=lambda x: x[0])
+        return [x[1] for x in self.comments[s1:e1]]
+
     def parse(self):
         pre_tokens = 0
         while self.con_str:
             for regex, rl_meta in self.rules:
                 if m := regex.match(self.con_str):
+                    self.current_comment = self._load_comments(pre_tokens, pre_tokens+len(m.group()))
                     yield rl_meta, self._back2str(pre_tokens, m.group()), self._back2strs(pre_tokens, m, regex.groups)
                     pre_tokens += len(m.group())
                     self.con_str = self.con_str[m.end():]
@@ -236,18 +248,18 @@ class Rust2H:
         raise Exception(f"unknown token: {instr}")
 
     def __init__(self, infile, outfile):
-        self.outf = sys.stdout
+        self.outf = io.StringIO()
         self.inf = sys.stdin
         self.outfile = outfile
         if infile:
             self.inf = open(infile, "r", encoding="utf-8")
-        if outfile:
-            self.outf = io.StringIO()
+        self._enum_fields = []
+        self._enum_name = ''
 
     def _trans_type(self, ss, has_err):
-        maping_tbl = { "String": "RustString", "u32": "uint32_t", "i32":"int", "u64": "uint64_t", "i64": "int64_t", "usize": "size_t",
-                       "isize": "ssize_t", "f32": "float", "f64": "double", "u8": "uint8_t", "i8": "int8_t", "u16": "uint16_t",
-                       "i16": "int16_t", "byte": "uint8_t"}
+        maping_tbl = {"String": "RustString", "u32": "uint32_t", "i32":"int", "u64": "uint64_t", "i64": "int64_t", "usize": "size_t",
+                      "isize": "ssize_t", "f32": "float", "f64": "double", "u8": "uint8_t", "i8": "int8_t", "u16": "uint16_t",
+                      "i16": "int16_t", "byte": "uint8_t"}
         if m := re.match(r"Vec<(.+)>", ss):
             return f"RustVec<{self._trans_type(m.group(1), has_err)}>"
         if m := re.match(r"Option<(.+)>", ss):
@@ -259,73 +271,142 @@ class Rust2H:
             return f"<ERROR_TYPE({ss})>"
         return ss
 
+    def _build_enum_st(self, has_err):
+        tps = [(v[0], self._trans_type(v[1], has_err), v[2]) for v in self._enum_fields if v[1]]
+        # comma_tps = ", ".join([v[1] for v in tps])
+        current_st = f"\ttype_t type;\n"
+        # current_st += f"\t[[no_unique_address]] rust2h_align<{comma_tps}>::pad_t reserved;\n"
+        current_st += "\tunion {\n"
+        used_varnames = set()
+        used_vartypes = set()
+        for en, vt, cmt in tps:
+            if m := re.search(r'var=(\w+)', cmt):
+                varname = m.group(1)
+            else:
+                varname = en.lower()
+            if (varname not in used_varnames) and (vt not in used_vartypes):
+                used_varnames.add(varname)
+                used_vartypes.add(vt)
+                current_st += f"\t\t{vt} {varname};\n"
+        current_st += "\t};\n"
+        current_st += "};\n"
+        return current_st
+
     def translate(self):
-        rules = [
+        ATTRS = 10
+        STRUCT_START = 20
+        STRUCT_FIELD = 30
+        STRUCT_FIELD1 = 31
+        STRUCT_END = 40
+        ENUM_START = 50
+        ENUM_FIELD = 60
+        USE_1 = 100
+        USE_2 = 101
+        rules = {
             # struct meta like #[derive(Serialize, Deserialize, Debug, Clone)]
-            "OP{#} OP{[} IDEN OP{(} (IDEN (?:OP{=}[STR2|NUM])?  OP{,})* IDEN (?:OP{=}[STR2|NUM])? OP{)} OP{]}",
+            ATTRS: "OP{#} OP{[} IDEN OP{(} (IDEN (?:OP{=}[STR2|NUM])?  OP{,})* IDEN (?:OP{=}[STR2|NUM])? OP{)} OP{]}",
             # start a pub? struct.
-            "(KW{pub})? KW{struct} (IDEN) OP<{>",
+            STRUCT_START: "(KW{pub})? KW{struct} (IDEN) OP<{>",
             # struct field
-            "(KW{pub})? (IDEN) OP{:} ((?:IDEN OP{::})*IDEN (OP{<} (?:IDEN OP{::})* IDEN OP{>})?) OP{,}?",
+            STRUCT_FIELD: "(KW{pub})? (IDEN) OP{:} ((?: IDEN OP{::})*IDEN (OP{<} (?:IDEN OP{::})* IDEN OP{>})?) (?: OP{,} | (?= OP<}> ) )",
+            STRUCT_FIELD1: "(KW{pub})? (IDEN) OP{:} ((?: IDEN OP{::})*IDEN OP{<} IDEN OP{,} IDEN OP{>}) OP{,}?",
             # struct end
-            "OP<}> OP{;}?",
-            "KW{use} (?: OP{::} KW{crate} OP{::})? (?:IDEN OP{::})* IDEN OP{;}",
-            "KW{use} (?: OP{::} KW{crate} OP{::})? (?:IDEN OP{::})* OP<{> (IDEN OP{,})* IDEN  OP<}> OP{;}",
-        ]
+            STRUCT_END: "OP<}> OP{;}?",
+
+            ENUM_START: "(KW{pub})? KW{enum} (IDEN) OP<{>",
+            ENUM_FIELD: "(IDEN) (?: OP<(> (IDEN) OP<)> )? OP{,}?",
+            USE_1: "KW{use} (?: OP{::} KW{crate} OP{::})? (?:IDEN OP{::})* IDEN OP{;}",
+            USE_2: "KW{use} (?: OP{::} KW{crate} OP{::})? (?:IDEN OP{::})* OP<{> ( IDEN OP{,})* IDEN  OP<}> OP{;}",
+        }
         instr = self.inf.read()
         p = MyParser(instr, rules, Rust2H.get_token)
-        print("#pragma once", file=self.outf)
-        print("#include \"rust/rust-common.h\"\n", file=self.outf)
-
-        ST_INIT, ST_STRUCT, ST_END = 0, 1, 2
+        ST_INIT, ST_STRUCT, ST_END, ST_ENUM = 0, 1, 2, 3
         state = ST_INIT
         attrs = []
         should_trans = False
         current_st = ""
         has_err = [0]
-        for idx,ss,grps in p.parse():
-            if idx >= 4:
+        for tp, ss, grps in p.parse():
+            if tp in (USE_1, USE_2):
                 continue
-            if idx == 0 and state == ST_INIT:
-                attrs.append(ss)
-            elif idx == 1 and state == ST_INIT:
+            if tp == ATTRS:
+                if state == ST_INIT:
+                    attrs.append(ss)
+            elif tp == STRUCT_START and state == ST_INIT:
                 should_trans = grps[0] == "pub" and any(["repr(C)" in x for x in attrs])
                 current_st += f"struct {grps[1]} {{\n"
                 state = ST_STRUCT
                 if not should_trans:
                     print(f"Skip struct {grps[1]} since it's not repr(C)", file=sys.stderr)
-            elif idx == 2 and state == ST_STRUCT:
+            elif tp == STRUCT_FIELD and state == ST_STRUCT:
                 current_st += "\t"
                 current_st += self._trans_type(grps[2], has_err)
                 current_st += f" {grps[1]};\n"
-            elif idx == 3 and state == ST_STRUCT:
-                current_st += "};\n"
-                state = ST_INIT
+            elif tp == STRUCT_END and state in (ST_STRUCT, ST_ENUM):
+                current_st += "%s};\n" % ("\t" if state == ST_ENUM else "")
                 attrs = []
                 if should_trans:
                     if has_err[0]:
-                        print(f"Warning: the struct has unknown type of fields, please fixit and retry:\n{current_st}", file=sys.stderr)
+                        print(f"Warning: the struct/enum has unknown type of fields, please fixit and retry:\n{current_st}", file=sys.stderr)
                     else:
+                        if state == ST_ENUM:
+                            current_st += self._build_enum_st(has_err)
                         print(current_st, file=self.outf)
                 current_st = ""
                 has_err = [0]
+                state = ST_INIT
+            elif tp == ENUM_START and state == ST_INIT:
+                should_trans = grps[0] == "pub" and any(["repr(C)" in x for x in attrs])
+                current_st += f"struct {grps[1]} {{\n\tenum type_t : int {{\n"
+                state = ST_ENUM
+                self._enum_fields = []
+                self._enum_name = grps[1]
+                if not should_trans:
+                    print(f"Skip enum {grps[1]} since it's not repr(C)", file=sys.stderr)
+            elif tp == ENUM_FIELD and state == ST_ENUM:
+                converted = re.sub(r'[A-Z]', lambda x: '_' + x.group().lower(), grps[0])
+                grps[0] = re.sub(r'^_', '', converted).upper()
+                self._enum_fields.append([grps[0], grps[1], "\n".join(p.current_comment)])
+                current_st += f"\t\t{grps[0]},\n"
+            elif tp == STRUCT_FIELD1 and state == ST_STRUCT:
+                assert not should_trans, "complex member should only be in ignored struct"
+            else:
+                assert False, f"unkown branch {tp=} {state=}"
         self.flush()
 
     def flush(self):
+        content = self.outf.getvalue()
+        if any([c for c in content if ord(c) > 127]):
+            encoding = "utf-8-sig"
+        else:
+            encoding = "utf-8"
+        header = "#pragma once\n"
+        if "RustOption" in content:
+            header += """#include "rust/rust-common.h"\n"""
+        else:
+            header += """#include "rust/rust_spt.h"\n"""
+        content = header + "\n" + content
+        self.outf.close()
+        self.inf.close()
+
         if self.outfile:
             changed = True
             try:
                 with open(self.outfile, "r", encoding="utf-8") as f:
-                    changed = f.read() != self.outf.getvalue()
+                    rd = f.read()
+                    if rd.startswith('\ufeff'):
+                        rd = rd[1:]
+                    changed = rd != content
             except FileNotFoundError:
                 pass
             if changed:
-                with open(self.outfile, "w", encoding="utf-8") as f:
-                    f.write(self.outf.getvalue())
+                with open(self.outfile, "w", encoding=encoding) as f:
+                    f.write(content)
             else:
                 print(f"No changes for file {self.outfile}", file=sys.stderr)
-        self.outf.close()
-        self.inf.close()
+        else:
+            sys.stdout.write(content)
+            sys.stdout.flush()
 
 
 if __name__ == "__main__":
